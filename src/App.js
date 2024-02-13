43831804843d3e12eea2159b04432720e646ffb7
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2024-02-13 17:04:14 +0000

    starting temporary pages

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="box-content">
+      <h3 className="me">Mandresy ANDRI</h3>
+      <h4 className="location">Vanves, France</h4>
+      <div className="buttons">
+        <button>LinkedIn</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
